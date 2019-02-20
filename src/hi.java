6965f72befdcd1291f8d3f6162a6891aa169ae30
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 01:49:26 +0800

    Merge branch 'master' into chen
    # Conflicts:
    #	src/hello.java

--- src/hi.java
+++ src/hi.java
@@ -3,8 +3,9 @@ public class hi {
     public static void main(String[] args) {
         System.out.println("dsffsdfsdf");
         System.out.println("dsffsdfsdf");
-        System.out.println("dsffsdfsdf");
-        System.out.println("dsffsdfsdf");
+        System.out.println("dfdsf");
+        System.out.println("dsffgffggggsdfsdf");
+
 
     }
 
