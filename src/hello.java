af7676d73f30a32f351e39aee3138237fd942b77
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 01:23:42 +0800

    haha

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,13 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hellsdffdds");
         System.out.println("gagag");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
     }
 
     public void chen(){
