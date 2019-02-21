d5f8c0b582c305750576f807e5382e587ab3a008
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 13:11:30 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/bbbb.java

--- src/bbbb.java
+++ src/bbbb.java
@@ -1,13 +1,6 @@
 public class bbbb {
 
-    public void chen(){
-
-        System.out.println("bbbbbbbbbbbbbbbbbbbbb");
-    }
-
-    public void chen1(){
-        System.out.println("ssdfsdfsfdg");
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println("bbbbbbbbbbbbbbbbbbbbb");
-    }
+   public void fanfan(){
+       System.out.println("git hub hao ma fan");
+   }
 }
