69295652edded9e4acca1d9d9e26b815f1f972ca
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 13:27:54 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/bbbb.java

--- src/bbbb.java
+++ src/bbbb.java
@@ -8,16 +8,5 @@ public class bbbb {
        System.out.println("dfsdfs");
    }
 
-    public void test1(){
-        System.out.println("dfsdfs");
-    }
-    public void test2(){
-        System.out.println("dfsdfs");
-    }
-    public void test3(){
-        System.out.println("dfsdfs");
-    }
-    public void test4(){
-        System.out.println("dfsdfs");
-    }
+
 }
