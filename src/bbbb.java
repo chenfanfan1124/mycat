c94d48900cce9ab950c9ae960ee715b8299a717e
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 10:30:08 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat into chen
    # Conflicts:
    #	src/hello.java

--- src/bbbb.java
+++ src/bbbb.java
@@ -1,8 +1,12 @@
 public class bbbb {
     public static void main(String[] args) {
         System.out.println("sdgdfsgsfdggfdsgsfdgsfgsfdg");
-        System.out.println("sdgdfsgsfdggfdsgsfdgsfgsfdg");
-        System.out.println("sdgdfsgsfdggfdsgsfdgsfgsfdg");
 
+
+    }
+    public void chen(){
+        System.out.println("ssdfsdfsfdg");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbbbbb");
     }
 }
