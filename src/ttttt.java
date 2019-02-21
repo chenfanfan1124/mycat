6cce564a24df76d034e246b100a638670f9eb90a
tom_glb <goodMorning_glb@atguigu.com>
2019-02-21 20:26:07 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/ttttt.java

--- src/ttttt.java
+++ src/ttttt.java
@@ -1,23 +1,18 @@
 public class ttttt {
 
+    public static void main2(String[] args) {
+        System.out.println("fsdfsd");
+    }
+
     public static void main(String[] args) {
+        System.out.println("fsdfsd");
+    }
+    public static void main1(String[] args) {
+        System.out.println("fsdfsd");
+    }
+    public static void main4(String[] args) {
         System.out.println("fsdfsd");
     }
 
 
-   public void demo1(){
-        System.out.println("hahaha");
-    }
-
-    public void demo2(){
-        System.out.println("hahaha");
-    }
-
-
-    public void demo3(){
-        System.out.println("hahaha");
-    }
-    public void demo4(){
-        System.out.println("hahaha");
-    }
 }
