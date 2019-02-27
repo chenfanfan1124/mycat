9939b08e60ccc45f938c8cfdfd00fd405c54f225
tom_glb <goodMorning_glb@atguigu.com>
2019-02-27 23:57:16 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/chenfafna.java

--- src/chushi.java
+++ src/chushi.java
@@ -6,6 +6,7 @@ public class chushi {
 
     public static void main5(String[] args) {
         System.out.println("初始化了");
+
     }
 
 
--- src/kan.java
+++ src/kan.java
@@ -1,6 +1,7 @@
 public class kan {
 
     public static void main(String[] args) {
+        System.out.println("fdf");
 
     }
 }
