69ebaa3407564b302e059668d143973be8a420c7
tom_glb <goodMorning_glb@atguigu.com>
2019-02-27 22:13:03 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/chenfafna.java

--- src/chenfafna.java
+++ src/chenfafna.java
@@ -2,8 +2,14 @@ public class chenfafna {
 
 
     public static void main(String[] args) {
-        System.out.println("fan");
-        System.out.println("fan");
+        System.out.println("ccc");
+        System.out.println("ccc");
+        System.out.println("ccc");
+        System.out.println("ccc");
+        System.out.println("ccc");
+        System.out.println("ccc");System.out.println("ccc");
+        System.out.println("ccc");
+
 
 
 
