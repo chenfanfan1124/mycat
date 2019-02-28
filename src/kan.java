c46ef32c7f8dcca3c550c0661ef29fb3069d56e3
tom_glb <goodMorning_glb@atguigu.com>
2019-02-28 20:05:33 +0800

    Merge branch 'master' of https://github.com/chenfanfan1124/mycat
    # Conflicts:
    #	src/chenfafna.java

--- src/kan.java
+++ src/kan.java
@@ -1,7 +1,44 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class kan {
 
     public static void main(String[] args) {
-        System.out.println("fdf");
 
+        Chen c=new Chen();
+        Chen c2=fan();
+        c.setCc("fdfsd");
+
+
+        Object wu = Optional.of(c).map(s ->
+            s.getCc()
+        ).orElse("fd");
+
+        Object wu1 = Optional.ofNullable(c2).map(s ->
+                 c2.getCc()
+        ).orElse("无");
+
+
+        System.out.println(wu);
+        System.out.println(wu1);
+    }
+
+    public static Chen fan(){
+        return null;
     }
 }
+
+class Chen{
+    private String cc;
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+}
+
